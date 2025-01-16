c2caf9e4cba308833c9066ef214ec446de4a6663
pkd <paulkronlund@gmail.com>
2025-01-16 19:19:28 +0100

    format fix

--- .quartz/quartz.config.ts
+++ .quartz/quartz.config.ts
@@ -21,7 +21,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Space Mono",
+        body: "Schibsted Grotesk",
         code: "Space Mono",
       },
       colors: {
